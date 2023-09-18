d2405dab07eaee757dd75a708ca405787e358a10
Pavlo.Moroz <55598887+PaulMoroz@users.noreply.github.com>
2023-09-18 09:02:59 +0300

    Add files via upload

--- remove_all_instances.py
+++ remove_all_instances.py
@@ -6,9 +6,16 @@ aws_secret_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
 region_name = os.environ.get('AWS_REGION')  # Replace with your preferred region
 
 # Check if all required environment variables are set
-if not (aws_access_key and aws_secret_key and region_name):
-    raise ValueError("AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, and AWS_REGION environment variables must be set.")
 
+# Check if all required environment variables are set
+if not (aws_access_key):
+    raise ValueError("AWS_ACCESS_KEY_ID environment variables must be set.")
+
+if not (aws_secret_key):
+    raise ValueError("AWS_SECRET_ACCESS_KEY environment variables must be set.")
+
+if not (region_name):
+    raise ValueError("AWS_REGION environment variables must be set.")
 # Create a Boto3 EC2 client
 ec2_client = boto3.client('ec2', aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_key, region_name=region_name)
 
