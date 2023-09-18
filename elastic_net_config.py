705dceb0d039d6c356260769036282403a017cc3
Pavlo.Moroz <55598887+PaulMoroz@users.noreply.github.com>
2023-09-18 08:44:15 +0300

    Add files via upload

--- elastic_net_config.py
+++ elastic_net_config.py
@@ -1,9 +1,12 @@
 import boto3
 import os
+
 # Read AWS credentials and configuration from environment variables
 aws_access_key = os.environ.get('AWS_ACCESS_KEY_ID')
 aws_secret_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
 region_name = os.environ.get('AWS_REGION')  # Replace with your preferred region
+elastic_ip = '16.171.222.150'  # Replace with your Elastic IP address
+instance_name = 'poll-app'
 
 # Check if all required environment variables are set
 if not (aws_access_key and aws_secret_key and region_name):
@@ -16,7 +19,14 @@ ec2_client = boto3.client('ec2', aws_access_key_id=aws_access_key, aws_secret_ac
 response = ec2_client.describe_addresses(PublicIps=[elastic_ip])
 allocation_id = response['Addresses'][0]['AllocationId']
 
-# Associate the Elastic IP with the EC2 instance
-ec2_client.associate_address(InstanceId=instance_id, AllocationId=allocation_id)
+# Get a list of EC2 instances with the specified name tag
+instances = ec2_client.describe_instances(Filters=[{'Name': 'tag:Name', 'Values': [instance_name]}])
 
-print(f"Associated Elastic IP {elastic_ip} with EC2 instance {instance_id}")
+for reservation in instances['Reservations']:
+    for instance in reservation['Instances']:
+        instance_id = instance['InstanceId']
+        
+        # Associate the Elastic IP with the EC2 instance
+        ec2_client.associate_address(InstanceId=instance_id, AllocationId=allocation_id)
+        
+        print(f"Associated Elastic IP {elastic_ip} with EC2 instance {instance_id}")
